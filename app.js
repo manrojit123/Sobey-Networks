498418aabe0ab14605990c54e8a9459296891435
manrojit123 <sayami.rojit@gmail.com>
2020-09-09 00:17:07 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// var headers= new Headers();
-// headers.append('Access-Control-Allow-Origin', '*');
-// headers.append('Access-Control-Allow-Credentials', 'true');
-// window.googleDocCallback = function () { return true; };
+ var headers= new Headers();
+ headers.append('Access-Control-Allow-Origin', '*');
+ headers.append('Access-Control-Allow-Credentials', 'true');
+ window.googleDocCallback = function () { return true; };
 
 const url = 'https://script.googleusercontent.com/a/macros/sobeynetworks.com/echo?user_content_key=d1QZ1Ni0KRwJnsTfjg8K1IqujKdZ7oIPPxlg75eZ-COvMV3szDL-EpNJhv4ejnHt5UeONRHYiqt6jgL0eJKHS1b2hBPN6J1Rm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_DlKiWSSlAchbIMgc_M8gHczY6gFn3TpwxbbXQd4eZLD0xytt0Wwav5zQU11hD0BOQdB0Y36PrStnE09tV1zENtjMiJApGOQLy_UtLp85HBlkNOJNgDeheA&lib=MqlHEbEmOKzi3tGytF7ISScVbR6dNk9pF';
 window.addEventListener('DOMContentLoaded',getData);
