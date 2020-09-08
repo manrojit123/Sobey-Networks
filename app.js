07f79e39233b899a61cc45cef776427a9cf85cd5
manrojit123 <sayami.rojit@gmail.com>
2020-09-09 00:16:00 +0545

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // headers.append('Access-Control-Allow-Credentials', 'true');
 // window.googleDocCallback = function () { return true; };
 
-const url = 'https://script.googleusercontent.com/macros/echo?user_content_key=sy73SzVgR1O0kM7_LaceHEzNhP51ml_XG2n1LxI1YZ3Hfksgvn7OwyeiDI8ObiLOFZ_V2Gehi-5oHTUGfO77kjYnGjU5odI8m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDYK1A3iZreOvKx6J8keU1oLj7U1PL35plE_oBSHtr6n-fMkMBud2p2Z_kcKr-1V1Q&lib=M4lIpecSdKMedJK1gjj5-kdSIthR4QL2e';
+const url = 'https://script.googleusercontent.com/a/macros/sobeynetworks.com/echo?user_content_key=d1QZ1Ni0KRwJnsTfjg8K1IqujKdZ7oIPPxlg75eZ-COvMV3szDL-EpNJhv4ejnHt5UeONRHYiqt6jgL0eJKHS1b2hBPN6J1Rm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_DlKiWSSlAchbIMgc_M8gHczY6gFn3TpwxbbXQd4eZLD0xytt0Wwav5zQU11hD0BOQdB0Y36PrStnE09tV1zENtjMiJApGOQLy_UtLp85HBlkNOJNgDeheA&lib=MqlHEbEmOKzi3tGytF7ISScVbR6dNk9pF';
 window.addEventListener('DOMContentLoaded',getData);
 const output = document.querySelector('.optput');
 function getData(){
